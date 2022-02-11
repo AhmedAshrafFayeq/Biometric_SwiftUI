dda0259ac365ff6bae8c938e3bc12e5346d4becf
Ahmed Fayeq <ahmedashraffayeq@gmail.com>
2022-02-11 19:24:49 +0200

    initial commit

---
+++ Biometric_SwiftUI/Biometric_SwiftUI/Biometric_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Biometric_SwiftUIApp.swift
+//  Biometric_SwiftUI
+//
+//  Created by Ahmed Fayeq on 11/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct Biometric_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
