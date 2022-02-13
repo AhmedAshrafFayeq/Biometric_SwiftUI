6dbe4ab0e001b85f89374af91136bc60f555ec41
Ahmed Fayeq <ahmedashraffayeq@gmail.com>
2022-02-13 18:55:32 +0200

    authenticate user with FaceID and simple SwiftUI view

--- Biometric_SwiftUI/Biometric_SwiftUI/ContentView.swift
+++ Biometric_SwiftUI/Biometric_SwiftUI/ContentView.swift
@@ -6,12 +6,41 @@
 //
 
 import SwiftUI
+import LocalAuthentication
 
 struct ContentView: View {
+    
+    @State private var isUnlocked = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            if isUnlocked {
+                Text("Unlocked")
+            }else{
+                Text("Locked")
+            }
+        }
+        .onAppear(perform: authenticateUser)
     }
+    
+    func authenticateUser(){
+        let context = LAContext()
+        var error: NSError?
+        
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error){
+            let reason = "we need to access your face Id"
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason){success, authenticationFailed in DispatchQueue.main.async {
+                if success{
+                    self.isUnlocked = true
+                }else{
+                }
+            }
+                
+            }
+        }else{
+        // can not use this feature
+    }
+}
 }
 
 struct ContentView_Previews: PreviewProvider {
